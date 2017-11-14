e38fb7f709cd2491d3fb758eff8e3ccf6daa282d
scurveedog <33649535+scurveedog@users.noreply.github.com>
2017-11-13 23:53:46 -0900

    Add files via upload

--- reducer.c
+++ reducer.c
@@ -15,65 +15,58 @@
  *
  * Invocation:
  * 	Pipe some data to exec
- *		$> cat token-out.txt | ./readLine | sort -k1,1
+ *		$> cat token-out.txt|./readFromStdin|sort -k1,1|./reducer
  */
 
 #include <stdio.h>
 #include <string.h>
-
 #include <stdlib.h>
 
 #define ARRAYSZ(x) (sizeof x/sizeof x[0])
 
 int main(void){
-
 	char *token, *linePtr;
-	char count[] = "1";
 	/* Arrays to hold returns from strtok. */
 	char word[120] = {0};
 	char currentWord[120] = {0};
 	/* delimiters used by strtok. */
-	const char delim[] = "\t";
-	/* Assumes word length < 40 chars. */
+	const char delim[] = "\n\t ";
+	/* Assumes max word length <= 120 chars. */
 	long unsigned int space = 120;
 	int currentCount = 1, i = 0;
-	/* Used to verify result of getline() call. */
-	int lineBytes = 0;
-	
-	linePtr = (char *)malloc(space);
 
+	linePtr = (char *)malloc(space);
 	do{
-			(lineBytes = getline(&linePtr, &space, stdin)) != -1;
+		 	getline(&linePtr, &space, stdin);
 			/* Get token1 (word) key */
 			token = strtok(linePtr, delim);
 			memcpy(word, token, strlen(token));
 			
-			/* Compare arrays for equality.  If == 0, Means we 
+			/* Compare arrays for equality.  If == 0, Means we
 			 * have more than one instance of 'word' in input. */
 			if(strcmp(word, currentWord) == 0){
 				currentCount += 1;
 			}
 			else{
 				if(currentWord[0] != 0)
-				   /* print stats for prev. word */
+					/* prn stats for prev. word. */
 					printf("\n%s\t%d", currentWord, currentCount);
-					
+				
 				/* Reset currentWord array. */
 				memset(currentWord, 0, ARRAYSZ(currentWord));
-
+				
 				memcpy(currentWord, word, strlen(word));
-				currentCount = atoi(count);
+				currentCount = 1;
 			}
-			/* Get token2 (value). If no more lines exit! */
-			if((token = strtok(NULL, delim)) == NULL)
-				continue;
-
 			memset(word, 0, ARRAYSZ(word));
-
+			
+			/* Get token2 (value). If no more lines exit. */
+			token = strtok(NULL, delim);
+			
 		}while(token);
 		
-		/* prints last data before prog end */
+		/* Prints last data before prog end */
 		printf("\n%s\t%d", currentWord, currentCount);
-
+		
 	return(0);
 }
